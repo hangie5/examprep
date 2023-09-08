0cc37e963906f601449516d1febcf2838167b511
hangie5 <142239368+hangie5@users.noreply.github.com>
2023-09-08 09:41:10 +0200

    Update print_reverse_alphabet.c

--- exam42/exam42/print_reverse_alphabet/print_reverse_alphabet.c
+++ exam42/exam42/print_reverse_alphabet/print_reverse_alphabet.c
@@ -18,3 +18,80 @@ int main(void)
     write(1, "\n", 1);
     return 0;
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#include <unistd.h>
+
+void print_reverse_alphabet(void)
+{
+char c;
+
+c = 'z';
+while(c <= 'a')
+{
+write(1, &c, 1);
+c--;
+}
+}
+
+int main(void)
+{
+print_reverse_alphabet()
+write(1, "\n", 1);
+return (0);
+}
+
+
+
+
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+
+#include <unistd.h>
+
+void print_reverse_alphabet(void)
+{
+char c;
+
+c = 'z';
+while(c <= 'a')
+{
+write(1, &c, 1);
+c--;
+}
+}
+
+int main(void)
+{
+print_reverse_alphabet();
+write(1 ,"\n", 1);
+return(0);
+}
